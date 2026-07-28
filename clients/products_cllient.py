42d3887f3d04468a126b83b0ba5f0006e13dbc7a
EgorKhodakov <khodakov91@yandex.ru>
2026-07-28 17:14:13 +0300

    feat: add products_client

---
+++ clients/products_cllient.py
@@ -0,0 +1,23 @@
+from httpx import Response
+
+from clients.base_client import BaseClient
+
+
+class ProductsClient(BaseClient):
+
+    def get_product_list(self) -> Response:
+        """
+        Получение списка товаров
+        :return: объект httpx Response
+        """
+        return self._request("GET", "/v1/products")
+
+
+    def get_product_by_id(self, product_id: int) -> Response:
+        """
+        Получение товара по id
+        :param product_id: уникальный id товара
+        :return: объект httpx Response
+        """
+        return self._request("GET", f"/v1/products/{product_id}")
+
