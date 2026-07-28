13a42ff82f8f88d53deedc5c7c2da17ef978ebaa
EgorKhodakov <khodakov91@yandex.ru>
2026-07-28 15:51:10 +0300

    feat: add base_client

---
+++ clients/base_client.py
@@ -0,0 +1,16 @@
+import httpx
+
+
+class BaseClient:
+    """
+    Base class for all clients.
+    """
+    def __init__(self, client: httpx.Client):
+        self.client = client
+
+    def _request(self, method: str, endpoint: str, **kwargs) -> httpx.Response:
+        response = self.client.request(method, endpoint, **kwargs)
+
+        response.raise_for_status()
+
+        return response
