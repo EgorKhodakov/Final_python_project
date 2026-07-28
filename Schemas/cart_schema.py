01ef203e1ca7d0d4d0703e2f4626b1126c0b36ef
EgorKhodakov <khodakov91@yandex.ru>
2026-07-28 15:21:14 +0300

    feat: add cart_schema

---
+++ Schemas/cart_schema.py
@@ -0,0 +1,24 @@
+from typing import List
+
+from pydantic import BaseModel
+
+
+class CartSchema(BaseModel):
+    """
+    Модель товара расположенного в корзине
+    """
+    product_id: int
+    quantity: int
+
+
+class CartResponseSchema(BaseModel):
+    """
+    Модель ответа запроса на получение корзины
+    """
+    items: List[CartSchema]
+    totalPriceCents: str
+    subtotalCents: str
+    discountCents: str
+    appliedPromocode: str
+    comboDiscountApplied: bool
+    expiresAt: str
