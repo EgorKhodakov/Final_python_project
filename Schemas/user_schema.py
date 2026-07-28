880dc55d78c62b3a1ff43e13538df6a2ec24010c
EgorKhodakov <khodakov91@yandex.ru>
2026-07-28 10:12:33 +0300

    feat: add user_schema

---
+++ Schemas/user_schema.py
@@ -0,0 +1,11 @@
+from pydantic import BaseModel
+
+
+class CreateUserShema(BaseModel):
+    """
+    Модель запроса для создания пользователя
+    """
+    email: str
+    password: str
+    name: str
+
